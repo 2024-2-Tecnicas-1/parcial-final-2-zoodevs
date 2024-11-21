2a74fc5485d9e7923c1304dd350f4c97682521f5
User <user@email.com>
2024-11-21 18:49:19 -0500

    Deldin
    Clase Delfin

--- src/main/java/logicaNegocio/Delfin.java
+++ src/main/java/logicaNegocio/Delfin.java
@@ -1,5 +1,21 @@
 package logicaNegocio;
 
-public class Delfin {
+public class Delfin extends Animal {
+    
+    public Delfin(){
+       super("Delfin", TipoHabitat habitat.ACUATICO)
+    }
+
+    @Override
+    public String emitirSonido() {
+        return "Chirrido";
+    }
+
+    @Override
+    public String obtenerDieta() {
+        return "Pescado";
+    }
     // TODO: Aquí va tu código
 }
+
+
